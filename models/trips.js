3a2a5618d4b803bfd34d5eebd6bc27418439cdd1
DistantDig <jameswaller711@gmail.com>
2023-11-02 11:42:18 -0700

    added additional columns to trips.js

--- models/trips.js
+++ models/trips.js
@@ -10,6 +10,26 @@ Trips.init(
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
+        },
+        trip_budget: {
+            type: DataTypes.DECIMAL
+        },
+        traveller_amount: {
+            type: DataTypes.DECIMAL
+        },
+        traveller_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'traveller',
+                key: 'id'
+            }
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'location',
+                key: 'id'
+            }
         }
     },
     {
